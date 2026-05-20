32888d8884294a36c88837c68a11871af418f962
anfegomezver <anfegomezver@gmail.com>
2026-05-20 01:03:55 -0500

    Incorporación de accesibilidad

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   server: {
     port: 3000,
+    allowedHosts: ['plasma-relapsing-antivirus.ngrok-free.dev']
   },
   vite: {
     plugins: [tailwindcss()]
