7868a2ef05fda1662865bf7da5c7d533e708b3cf
RojasP7 <ferojasp@ufpso.edu.co>
2026-06-16 23:21:28 -0500

    Configure Astro for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  adapter: vercel(),
+
   integrations: [icon()],
+
   server: {
     port: 3000,
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
